82d20cf5d70e0f555573251335cca37d8bae0ee5
Entonyos <antonyossolomon@gmail.com>
2024-07-12 04:08:38 -0700

    database creation

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors=require('cors');
+const dotenv=require('dotenv');
+dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+mongoose
+    .connect(`mongodb://127.0.0.1:27017/Welcome`)
+    .then(() => {
+        console.log("DB Connected Successfully");
+    })
 
-
-app.listen(8000,()=>{
-    console.log("listening");
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on ${process.env.PORT}`);
 })
